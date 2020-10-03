tuxbox-fork-migrations/recycled-libstb-hal-tangos

save cover if available, report avfc to streaminfo2
Origin commit data
------------------
Commit: https://github.com/TangoCash/libstb-hal-tangos/commit/1d0861f700a4771cec698c713b0d96c617f23d6c
Author: TangoCash <eric@loxat.de>
Date: 2020-09-16 (Wed, 16 Sep 2020)


------------------
No further description and justification available within origin commit message!

------------------
This commit was generated by Migit

libarmbox/playback_libeplayer3.cpp
@@ -53,6 +53,7 @@ bool cPlayback::Open(playmode_t PlayMode)
 	last_size = 0;
 	nPlaybackSpeed = 0;
 	init_jump = -1;
+	avft = avformat_alloc_context();
 
 	if (!player)
 	{
@@ -116,7 +117,6 @@ bool cPlayback::Start(char *filename, int vpid, int vtype, int apid, int ac3, in
 	mAudioStream = 0;
 	mSubtitleStream = -1;
 	mTeletextStream = -1;
-	unlink("/tmp/.id3coverart");
 	std::string file;
 
 	if (*filename == '/')
@@ -832,11 +832,17 @@ uint64_t cPlayback::GetReadCount()
 
 AVFormatContext *cPlayback::GetAVFormatContext()
 {
-	return NULL;
+	if (player && player->container && player->container->selectedContainer)
+	{
+		player->container->selectedContainer->Command(player, CONTAINER_GET_AVCONTEXT, avft);
+	}
+	return avft;
 }
 
 void cPlayback::ReleaseAVFormatContext()
 {
+	avft->streams = NULL;
+	avft->nb_streams = NULL;
 }
 
 #if 0

dbt1/libstb-hal

libarmbox/playback_libeplayer3.cpp
@@ -54,6 +54,7 @@ bool cPlayback::Open(playmode_t PlayMode)
 	last_size = 0;
 	nPlaybackSpeed = 0;
 	init_jump = -1;
+	avft = avformat_alloc_context();
 
 	if (!player)
 	{
@@ -117,7 +118,6 @@ bool cPlayback::Start(char *filename, int vpid, int vtype, int apid, int ac3, in
 	mAudioStream = 0;
 	mSubtitleStream = -1;
 	mTeletextStream = -1;
-	unlink("/tmp/.id3coverart");
 	std::string file;
 
 	if (*filename == '/')
@@ -856,11 +856,17 @@ uint64_t cPlayback::GetReadCount()
 
 AVFormatContext *cPlayback::GetAVFormatContext()
 {
-	return NULL;
+	if (player && player->container && player->container->selectedContainer)
+	{
+		player->container->selectedContainer->Command(player, CONTAINER_GET_AVFCONTEXT, avft);
+	}
+	return avft;
 }
 
 void cPlayback::ReleaseAVFormatContext()
 {
+	avft->streams = NULL;
+	avft->nb_streams = NULL;
 }
 
 #if 0